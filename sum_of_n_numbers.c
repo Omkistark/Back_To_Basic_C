8210e1b848da33a92cab1e70e7c612a4009aca05
Omkar Dutta <omkar1620@gmail.com>
2020-12-28 23:20:41 +0530

    Sum of n integers Program Added!

---
+++ sum_of_n_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,count,sum=0;
+    printf("Enter Number of integers you want to add:\n");
+    scanf("%d",&count);
+    printf("Enter the numbers\n");
+    for (int i = 0; i < count; i++)
+    {
+        scanf("%d",&a);
+        sum+=a;
+    }
+    printf("\nSum of th %d numbers is %d",count,sum);
+    return 0;
+}
